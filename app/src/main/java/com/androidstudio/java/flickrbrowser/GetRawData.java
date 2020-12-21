7757738bab0010575abf52674587a2027b705668
yasithWimukthi <it19966922@my.sliit.lk>
2020-12-21 19:24:56 +0530

    Edit GetRawData Class

--- app/src/main/java/com/androidstudio/java/flickrbrowser/GetRawData.java
+++ app/src/main/java/com/androidstudio/java/flickrbrowser/GetRawData.java
@@ -15,6 +15,7 @@ enum DownloadStatus {IDLE,PROCESSING,NOT_INITIALIZED,FAILED_OR_EMPTY,OK}
 public class GetRawData extends AsyncTask<String,Void,String> {
 
     private static final String TAG = "GetRawData";
+    private static final String LOG_TAG = "GetRawData";
     private DownloadStatus mDownloadStatus;
 
     public GetRawData() {
@@ -43,16 +44,37 @@ public class GetRawData extends AsyncTask<String,Void,String> {
             StringBuilder result = new StringBuilder();
             reader = new  BufferedReader(new InputStreamReader(connection.getInputStream()));
 
+            String line;
+            while(null != (line = reader.readLine())){
+                result.append(line).append("/n");
+            }
+
+            mDownloadStatus = DownloadStatus.OK;
+            return result.toString();
+
         }catch( MalformedURLException e){
             e.printStackTrace();
         }catch(IOException | SecurityException e){
             e.printStackTrace();
         }
+        finally {
+            if (connection != null){
+                connection.disconnect();
+            }
+            if (reader != null){
+                try {
+                    reader.close();
+                }catch ( IOException e){
+                    Log.e(LOG_TAG, "doInBackground : error closing stream "+e.getMessage());
+                }
+            }
+        }
+        mDownloadStatus = DownloadStatus.FAILED_OR_EMPTY;
         return null;
     }
 
     @Override
     protected void onPostExecute(String s) {
-        super.onPostExecute(s);
+        //super.onPostExecute(s);
     }
 }
