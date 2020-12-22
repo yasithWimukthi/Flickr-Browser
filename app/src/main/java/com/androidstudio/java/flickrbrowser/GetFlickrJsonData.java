ab8496b0f6d51c44fce590e89b9751b6f7c1d299
yasithWimukthi <it19966922@my.sliit.lk>
2020-12-22 23:23:39 +0530

    Implement GetFlickrJsonData class

--- app/src/main/java/com/androidstudio/java/flickrbrowser/GetFlickrJsonData.java
+++ app/src/main/java/com/androidstudio/java/flickrbrowser/GetFlickrJsonData.java
@@ -1,5 +1,74 @@
 package com.androidstudio.java.flickrbrowser;
 
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class GetFlickrJsonData implements OnDownloadComplete{
 
+    private static final String TAG = "GetFlickrJsonData";
+
+    private final OnDataAvailable mCallback;
+    private List<Photo> mPhotoList = null;
+    private String mBaseUrl;
+    private String mLanguage;
+    private boolean mMatchAll;
+
+    public GetFlickrJsonData(OnDataAvailable callback, String baseUrl, String language, boolean matchAll) {
+        mCallback = callback;
+        mBaseUrl = baseUrl;
+        mLanguage = language;
+        mMatchAll = matchAll;
+    }
+
+    void executeOnSameThread (String searchCriteria){
+        Log.d(TAG, "executeOnSameThread: start");
+        String destinationUri = createUri(searchCriteria,mLanguage,mMatchAll);
+
+        GetRawData getRawData = new GetRawData(this);
+        getRawData.execute(destinationUri);
+        Log.d(TAG, "executeOnSameThread: end");
+    }
+
+    private String createUri(String searchCriteria, String language, boolean matchAll) {
+        return Uri.parse(mBaseUrl).buildUpon()
+                .appendQueryParameter("tags",searchCriteria)
+                .appendQueryParameter("tagmode",matchAll ? "ALL" : "ANY")
+                .appendQueryParameter("lang",language)
+                .appendQueryParameter("format","json")
+                .appendQueryParameter("nojsoncallback","1")
+                .build().toString();
+    }
+
+    @Override
+    public void onDownloadComplete(String data, DownloadStatus status) {
+
+        if (status == DownloadStatus.OK){
+            mPhotoList = new ArrayList<Photo>();
+
+            try{
+                JSONObject jsonData = new JSONObject(data);
+                JSONArray itemsArray = jsonData.getJSONArray("items");
+
+                for (int i = 0; i < itemsArray.length();i++){
+                    JSONObject photo = itemsArray.getJSONObject(i);
+                    String title = photo.getString("title");
+                    String author = photo.getString("author");
+                    String AuthorID = photo.getString("Author_id");
+                    String tags = photo.getString("tags");
+
+                    JSONObject media = photo.optJSONObject("media");
+                    String photoUrl = media.getString("m");
+
+                    String link = photoUrl.replaceFirst("_m.","_b.");
+                }
+            }
+        }
+    }
 }
