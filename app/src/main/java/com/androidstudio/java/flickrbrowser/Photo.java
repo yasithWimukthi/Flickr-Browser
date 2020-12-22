0b09aa84d56c2af7d4e57994a5d5abb117ec3cd3
yasithWimukthi <it19966922@my.sliit.lk>
2020-12-22 21:54:34 +0530

    Implement Photo class

---
+++ app/src/main/java/com/androidstudio/java/flickrbrowser/Photo.java
@@ -0,0 +1,55 @@
+package com.androidstudio.java.flickrbrowser;
+
+public class Photo {
+    private String mTitle;
+    private String mAuthor;
+    private String mAuthorId;
+    private String mLink;
+    private String mTag;
+    private String mImage;
+
+    public Photo(String mTitle, String mAuthor, String mAuthorId, String mLink, String mTag, String mImage) {
+        this.mTitle = mTitle;
+        this.mAuthor = mAuthor;
+        this.mAuthorId = mAuthorId;
+        this.mLink = mLink;
+        this.mTag = mTag;
+        this.mImage = mImage;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getAuthor() {
+        return mAuthor;
+    }
+
+    public String getAuthorId() {
+        return mAuthorId;
+    }
+
+    public String getLink() {
+        return mLink;
+    }
+
+    public String getTag() {
+        return mTag;
+    }
+
+    public String getImage() {
+        return mImage;
+    }
+
+    @Override
+    public String toString() {
+        return "Photo{" +
+                "mTitle='" + mTitle + '\'' +
+                ", mAuthor='" + mAuthor + '\'' +
+                ", mAuthorId='" + mAuthorId + '\'' +
+                ", mLink='" + mLink + '\'' +
+                ", mTag='" + mTag + '\'' +
+                ", mImage='" + mImage + '\'' +
+                '}';
+    }
+}
